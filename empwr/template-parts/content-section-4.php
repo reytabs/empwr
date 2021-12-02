a037fb0c7e2bd5e92ae0cca79b40c53a665a4fc8
Jan Marc Umali <20512632+marcumali@users.noreply.github.com>
2021-12-02 21:10:27 +0800

    ui adjustments desktop ..

--- empwr/template-parts/content-section-4.php
+++ empwr/template-parts/content-section-4.php
@@ -18,19 +18,26 @@
   </div>
   <div class="container">
     <div class="d-flex justify-content-center">
-      <div class="mx-5 px-5 text-center">
+      <div class="mx-5 px-2 text-center">
         <div class="w-64px h-64px rounded-10 bg-gray-3 position-relative mx-auto mb-4">
           <img src="<?php echo esc_url(get_stylesheet_directory_uri()); ?>/img/icons/icon-map-pin.png" class="position-absolute top-0 bottom-0 start-0 end-0 m-auto">
         </div>
         <div class="font-primary-bold text-color-1 mb-3 h5">Location</div>
         <div class="font-primary-bold text-primary h5">Dubai Silicon Oasis, UAE</div>
       </div>
-      <div class="mx-5 px-5 text-center">
+      <div class="mx-5 px-2 text-center">
         <div class="w-64px h-64px rounded-10 bg-gray-3 position-relative mx-auto mb-4">
           <img src="<?php echo esc_url(get_stylesheet_directory_uri()); ?>/img/icons/icon-phone.png" class="position-absolute top-0 bottom-0 start-0 end-0 m-auto">
         </div>
         <div class="font-primary-bold text-color-1 mb-3 h5">Whatsapp</div>
         <div class="font-primary-bold text-primary h5">+971503778204</div>
+      </div>
+      <div class="mx-5 px-2 text-center">
+        <div class="w-64px h-64px rounded-10 bg-gray-3 position-relative mx-auto mb-4">
+          <img src="<?php echo esc_url(get_stylesheet_directory_uri()); ?>/img/icons/icon-mail.png" class="position-absolute top-0 bottom-0 start-0 end-0 m-auto">
+        </div>
+        <div class="font-primary-bold text-color-1 mb-3 h5">Email</div>
+        <div class="font-primary-bold text-primary h5">ally@empwrhouse.com</div>
       </div>
     </div>
   </div>
