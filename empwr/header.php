934d5bc4b43bbfe9abe8fd4d97e50fe8e3d6ff60
Jan Marc Umali <20512632+marcumali@users.noreply.github.com>
2021-12-02 18:50:32 +0800

    add contact us page ..

--- empwr/header.php
+++ empwr/header.php
@@ -27,6 +27,22 @@
   <meta name="msapplication-TileColor" content="#ffffff">
   <meta name="theme-color" content="#ffffff">
   <?php wp_head(); ?>
+  <script type="text/javascript">
+  ( function($){
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 500);
+          return false;
+        }
+      }
+    });
+  }) (jQuery);
+  </script>
 </head>
 
 <body <?php body_class(); ?>>
--- empwr/page-landing.php
+++ empwr/page-landing.php
@@ -12,6 +12,7 @@ get_header();
 get_template_part( 'template-parts/content', 'section-1' );
 get_template_part( 'template-parts/content', 'section-2' );
 get_template_part( 'template-parts/content', 'section-3' );
+get_template_part( 'template-parts/content', 'section-4' );
 ?>
 <?php
 get_footer();
